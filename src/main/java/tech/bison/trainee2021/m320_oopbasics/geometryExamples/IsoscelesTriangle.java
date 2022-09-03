1b6d0a798ebf11e152d8a5abfc57244c26c21c97
johan <johan@DESKTOP-9OHV0HE.home>
2022-09-03 16:30:22 +0200

    impelement basic isosceles triangle features

--- src/main/java/tech/bison/trainee2021/m320_oopbasics/geometryExamples/IsoscelesTriangle.java
+++ src/main/java/tech/bison/trainee2021/m320_oopbasics/geometryExamples/IsoscelesTriangle.java
@@ -2,4 +2,23 @@ package tech.bison.trainee2021.m320_oopbasics.geometryExamples;
 
 public class IsoscelesTriangle {
 
+  private double base;
+  private double height;
+
+  public IsoscelesTriangle(double base, double height) {
+    this.base = base;
+    this.height = height;
+  }
+
+  public double getBase() {
+    return base;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+
+  public double getArea() {
+    return base * height / 2;
+  }
 }
--- src/test/java/tech/bison/trainee2021/m320_oopbasics/geometryExamples/IsoscelesTriangleTest.java
+++ src/test/java/tech/bison/trainee2021/m320_oopbasics/geometryExamples/IsoscelesTriangleTest.java
@@ -1,5 +1,61 @@
 package tech.bison.trainee2021.m320_oopbasics.geometryExamples;
 
-public class IsoscelesTriangleTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+
+public class IsoscelesTriangleTest {
+  @Test
+  void newIsoscelesTriangleWithBaseOne_getBase_isOne() {
+    IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle(1, 1);
+
+    double base = isoscelesTriangle.getBase();
+
+    assertThat(base).isOne();
+  }
+
+  @Test
+  void newIsoscelesTriangleWithBaseTwo_getBase_isTwo() {
+    IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle(2, 1);
+
+    double base = isoscelesTriangle.getBase();
+
+    assertThat(base).isEqualTo(2);
+  }
+
+  @Test
+  void newIsoscelesTriangleWithHeightOne_getBase_isOne() {
+    IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle(1, 1);
+
+    double height = isoscelesTriangle.getHeight();
+
+    assertThat(height).isOne();
+  }
+
+  @Test
+  void newIsoscelesTriangleWithHeightTwo_getHeight_isTwo() {
+    IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle(1, 2);
+
+    double height = isoscelesTriangle.getHeight();
+
+    assertThat(height).isEqualTo(2);
+  }
+
+  @Test
+  void newIsoscelesTriangleWithBaseHeightOne_getArea_isOneHalf() {
+    IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle(1, 1);
+
+    double area = isoscelesTriangle.getArea();
+
+    assertThat(area).isEqualTo(0.5);
+  }
+
+  @Test
+  void newRightTriangleWithAdjacentOppositeSideTwo_getArea_isTwo() {
+    IsoscelesTriangle isoscelesTriangle = new IsoscelesTriangle(2, 2);
+
+    double area = isoscelesTriangle.getArea();
+
+    assertThat(area).isEqualTo(2);
+  }
 }
