de4d2d954604ca8c889ac5a4012914ddeef08e2f
johan <johan@DESKTOP-9OHV0HE.home>
2022-09-03 15:59:53 +0200

    create basic square features

--- src/main/java/tech/bison/trainee2021/m320_oopbasics/geometryExamples/Square.java
+++ src/main/java/tech/bison/trainee2021/m320_oopbasics/geometryExamples/Square.java
@@ -2,4 +2,17 @@ package tech.bison.trainee2021.m320_oopbasics.geometryExamples;
 
 public class Square {
 
+  private double sideLength;
+
+  public Square(int sideLength) {
+    this.sideLength = sideLength;
+  }
+
+  public double getSideLength() {
+    return sideLength;
+  }
+
+  public double getArea() {
+    return Math.pow(sideLength, 2);
+  }
 }
--- src/test/java/tech/bison/trainee2021/m320_oopbasics/geometryExamples/SquareTest.java
+++ src/test/java/tech/bison/trainee2021/m320_oopbasics/geometryExamples/SquareTest.java
@@ -1,5 +1,44 @@
 package tech.bison.trainee2021.m320_oopbasics.geometryExamples;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 public class SquareTest {
 
+  @Test
+  void newSquareWithSideLengthOne_getSideLength_isOne() {
+    Square square = new Square(1);
+
+    double sideLength = square.getSideLength();
+
+    assertThat(sideLength).isOne();
+  }
+
+  @Test
+  void newSquareWithSideLengthTwo_getSideLength_isTwo() {
+    Square square = new Square(2);
+
+    double sideLength = square.getSideLength();
+
+    assertThat(sideLength).isEqualTo(2);
+  }
+
+  @Test
+  void squareWithSideLengthOne_getArea_isOne() {
+    Square square = new Square(1);
+
+    double area = square.getArea();
+
+    assertThat(area).isOne();
+  }
+
+  @Test
+  void squareWithSideLengthTwo_getArea_isFour() {
+    Square square = new Square(2);
+
+    double area = square.getArea();
+
+    assertThat(area).isEqualTo(4);
+  }
 }
