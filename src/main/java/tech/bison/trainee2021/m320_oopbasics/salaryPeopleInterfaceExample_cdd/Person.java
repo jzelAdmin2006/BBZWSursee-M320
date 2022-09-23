4e5df70c7d86ec803244ee076ae2b855d64400ae
johannes.zeller <johannes.zeller@DESKTOP-KO6JJPB.localdomain>
2022-09-23 10:47:03 +0200

    add example attributes for person

--- src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/Person.java
+++ src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/Person.java
@@ -1,5 +1,28 @@
 package tech.bison.trainee2021.m320_oopbasics.salaryPeopleInterfaceExample_cdd;
 
 public class Person {
+  private String lastName;
 
+  public Person(String lastName, String firstName) {
+    setLastName(lastName);
+    setFirstName(firstName);
+  }
+
+  private String firstName;
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 }
