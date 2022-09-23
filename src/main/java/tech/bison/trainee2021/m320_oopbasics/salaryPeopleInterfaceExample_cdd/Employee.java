4c7e23d0094e88f83e6dff3294e14cba1bd0b461
johannes.zeller <johannes.zeller@DESKTOP-KO6JJPB.localdomain>
2022-09-23 11:17:01 +0200

    implement employee members according to task https://bit.ly/3Ss7Fpr

--- src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/Employee.java
+++ src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/Employee.java
@@ -2,7 +2,28 @@ package tech.bison.trainee2021.m320_oopbasics.salaryPeopleInterfaceExample_cdd;
 
 public class Employee extends Person {
 
-  public Employee(String lastName, String firstName) {
+  private double salary;
+  private String company;
+
+  public Employee(String lastName, String firstName, double salary, String company) {
     super(lastName, firstName);
+    setCompany(company);
+    setSalary(salary);
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public void setCompany(String company) {
+    this.company = company;
+  }
+
+  public double getSalary() {
+    return salary;
+  }
+
+  public void setSalary(double salary) {
+    this.salary = salary;
   }
 }
