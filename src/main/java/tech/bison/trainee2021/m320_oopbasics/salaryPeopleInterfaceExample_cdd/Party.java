22c390cc010fd4ec4d56b50eccf8d9f63283ca77
johannes.zeller <johannes.zeller@DESKTOP-KO6JJPB.localdomain>
2022-09-23 10:57:25 +0200

    implement class needed for partymember member

---
+++ src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/Party.java
@@ -0,0 +1,17 @@
+package tech.bison.trainee2021.m320_oopbasics.salaryPeopleInterfaceExample_cdd;
+
+public class Party {
+  private String name;
+
+  public Party(String name) {
+    setName(name);
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
