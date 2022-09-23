b726227432344d12f401e9b69ede8f1f9d342ab6
johannes.zeller <johannes.zeller@DESKTOP-KO6JJPB.localdomain>
2022-09-23 11:07:44 +0200

    implement partymember members according to task https://bit.ly/3Ss7Fpr

--- src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/PartyMember.java
+++ src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/PartyMember.java
@@ -2,7 +2,18 @@ package tech.bison.trainee2021.m320_oopbasics.salaryPeopleInterfaceExample_cdd;
 
 public class PartyMember extends Person {
 
-  public PartyMember(String lastName, String firstName) {
+  private String party;
+
+  public PartyMember(String lastName, String firstName, String party) {
     super(lastName, firstName);
+    setParty(party);
+  }
+
+  public String getParty() {
+    return party;
+  }
+
+  public void setParty(String party) {
+    this.party = party;
   }
 }
