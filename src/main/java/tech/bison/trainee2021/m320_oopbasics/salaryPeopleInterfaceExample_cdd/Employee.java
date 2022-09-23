a87d1031eb2581b400fc52eabd9aa6921e469df6
johannes.zeller <johannes.zeller@DESKTOP-KO6JJPB.localdomain>
2022-09-23 10:48:48 +0200

    implement needed constructors

--- src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/Employee.java
+++ src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/Employee.java
@@ -2,4 +2,7 @@ package tech.bison.trainee2021.m320_oopbasics.salaryPeopleInterfaceExample_cdd;
 
 public class Employee extends Person {
 
+  public Employee(String lastName, String firstName) {
+    super(lastName, firstName);
+  }
 }
--- src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/PartyMember.java
+++ src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/PartyMember.java
@@ -2,4 +2,7 @@ package tech.bison.trainee2021.m320_oopbasics.salaryPeopleInterfaceExample_cdd;
 
 public class PartyMember extends Person {
 
+  public PartyMember(String lastName, String firstName) {
+    super(lastName, firstName);
+  }
 }
