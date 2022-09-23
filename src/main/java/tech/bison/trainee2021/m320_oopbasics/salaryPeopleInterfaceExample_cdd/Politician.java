76e4465ffb0aa3c22b422707afcaa569d8690ade
johannes.zeller <johannes.zeller@DESKTOP-KO6JJPB.localdomain>
2022-09-23 11:11:47 +0200

    add needed politician memebers according to task https://bit.ly/3Ss7Fpr

--- src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/Politician.java
+++ src/main/java/tech/bison/trainee2021/m320_oopbasics/salaryPeopleInterfaceExample_cdd/Politician.java
@@ -1,5 +1,29 @@
 package tech.bison.trainee2021.m320_oopbasics.salaryPeopleInterfaceExample_cdd;
 
-public class Politician {
+public class Politician extends PartyMember {
 
+  private double salary;
+  private String council;
+
+  public Politician(String lastName, String firstName, String party, String council) {
+    super(lastName, firstName, party);
+    setCouncil(council);
+    setSalary(salary);
+  }
+
+  public String getCouncil() {
+    return council;
+  }
+
+  public void setCouncil(String council) {
+    this.council = council;
+  }
+
+  public double getSalary() {
+    return salary;
+  }
+
+  public void setSalary(double salary) {
+    this.salary = salary;
+  }
 }
